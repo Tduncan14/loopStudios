468552f3fd916091dcaa26ed6b3eaacfed2a190e
tduncan14 <nbatre1@yahoo.com>
2024-02-15 23:17:32 -0800

    keep moving forward

--- loopstudios/src/App.js
+++ loopstudios/src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
+import Content from './Components/Content/Content';
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="app">
       <div className="container">
         <Navbar />
-
+        <Content />
+        <Footer />
       </div>
     </div>
   );
